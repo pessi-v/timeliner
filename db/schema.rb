fdc8a6cc6a7ccef75838dd6d97a0faf3fb9640e4
pessi-v <pessimo@posteo.net>
2026-02-25 19:52:33 +0100

    run migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_12_02_124741) do
+ActiveRecord::Schema[8.1].define(version: 2026_02_25_184938) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "sessions", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "ip_address"
+    t.datetime "updated_at", null: false
+    t.string "user_agent"
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_sessions_on_user_id"
+  end
 
   create_table "timelines", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -22,4 +31,14 @@ ActiveRecord::Schema[8.1].define(version: 2025_12_02_124741) do
     t.datetime "updated_at", null: false
     t.index ["timeline_data"], name: "index_timelines_on_timeline_data", using: :gin
   end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "email_address", null: false
+    t.string "password_digest", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email_address"], name: "index_users_on_email_address", unique: true
+  end
+
+  add_foreign_key "sessions", "users"
 end
